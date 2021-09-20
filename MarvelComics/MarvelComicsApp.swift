102c4a177abd5227c3c6d1ac4261a2c1de71e1bc
hamza jerbi <hamzon.jb@gmail.com>
2021-09-20 11:35:01 +0200

    Initial Commit

---
+++ MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Hamza on 20/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
