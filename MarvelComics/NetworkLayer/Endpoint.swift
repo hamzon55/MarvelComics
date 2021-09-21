45ee1fe689e47931ed05e0ecfef27dcc83af0a4c
hamza jerbi <hamzon.jb@gmail.com>
2021-09-21 18:15:58 +0200

    added endpoint example

---
+++ MarvelComics/NetworkLayer/Endpoint.swift
@@ -0,0 +1,14 @@
+//
+//  Endpoint.swift
+//  MarvelComics
+//
+//  Created by Hamza on 21/9/21.
+//
+
+import Foundation
+
+struct Endpoint {
+    
+    var path: String
+    var queryItems: [URLQueryItem] = []
+}
